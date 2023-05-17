89a87d67622176498159aa09782235661dcd4d94
Danamarie10 <dana.patrick10@outlook.com>
2023-05-17 11:22:18 -0400

    corrected first exercise

---
+++ IterationStatementsEx1/IterationStatementsEx1/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace IterationStatementsEx1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // for is the loop type
+            // int i is the initializer and variable
+            // i < 5 is the condition to which must be false for loop to continue
+            // i++ is the incremement at which the variable increases
+         for (int i = 0; i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+         for (int i = 0;i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+         for (int i = 0; i<5 ; i++) 
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 5: i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0;i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0;i<5 ; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i<5 ; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i<5 ; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for ( int i = 0; i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0;i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+        }
+    }
+}
